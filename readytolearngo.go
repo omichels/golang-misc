96b687d8fe2b6e84c4c07902c654fe91c2f6940c
Oliver Michels <oliver.michels@gmail.com>
2022-03-21 14:47:08 +0000

    init

---
+++ readytolearngo.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func main() {
+		fmt.Println("Looks like im am ready to learn Golang")
+		fmt.Println("my fav number is 42")
+		fmt.Println("However, i grab random numbers as well", rand.Intn(10))
+	fmt.Println(math.Pi)
+
+}
+
